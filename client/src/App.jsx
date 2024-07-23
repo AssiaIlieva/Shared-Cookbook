d9f41fabe98cd8da49d623d6954957414820ebc3
AssiaIlieva <ac17@abv.bg>
2024-07-23 20:25:16 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/header/Header';
+import { useState } from 'react';
 
+import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 
 function App() {
+  const [auth, setAuth] = useState({});
+
+  const loginSubmitHandler = (values) => {
+    console.log(values);
+  };
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler} />} />
       </Routes>
     </>
   );
