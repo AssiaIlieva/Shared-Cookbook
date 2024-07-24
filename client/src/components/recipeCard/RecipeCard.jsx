9905955fba2dfe7b096e0c9cf9767629ad82083e
AssiaIlieva <ac17@abv.bg>
2024-07-24 16:11:07 +0300

    Create RecipeCard component and its external css file

---
+++ client/src/components/recipeCard/RecipeCard.jsx
@@ -0,0 +1,29 @@
+// src/components/RecipeCard.js
+import React from 'react';
+
+import styles from './RecipeCard.module.css';
+
+const RecipeCard = () => {
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.card}>
+        <img src="https://noobcook.com/wp-content/uploads/2012/02/blackpeppersteak.jpg" alt="PepperSteak" className={styles.image} />
+      </div>
+      <div className="title">
+        <h1 className="border-bottom">Pepper steak</h1>
+      </div>
+      <div className="border" />
+      <div className="content">
+        <p>
+          In bibendum eros eget dolor gravida sed venenatis nisl sollicitudin.
+          <br />
+        </p>
+        <div className="button-link">
+          <a href="#">readmore</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
