4c3ec87e09567d85df9e0163b8e350cae061d43a
AssiaIlieva <ac17@abv.bg>
2024-07-28 15:09:09 +0300

    Update the layout of Login and Register form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -15,46 +15,48 @@ export default function Login({ loginSubmitHandler }) {
 
   return (
     <div className="top-content">
-      <div className="primary-content marRight30">
-        <div className="mid-panel">
-          <div className="mid-panel-content">
-            <div className="title">
-              <h1>login</h1>
-            </div>
-            <div className="border" />
-            <h2>Please, login</h2>
-            <form onSubmit={onSubmit}>
-              <div className="contact-form margin-top">
-                <label>
-                  <span>Email</span>
-                  <input
-                    className="input_text"
-                    id="email"
-                    name={loginFormKeys.email}
-                    type="text"
-                    onChange={onChange}
-                    value={values[loginFormKeys.email]}
-                  />
-                </label>
-                <label>
-                  <span>Password</span>
-                  <input
-                    className="input_text"
-                    id="password"
-                    name={loginFormKeys.password}
-                    type="password"
-                    onChange={onChange}
-                    value={values[loginFormKeys.password]}
-                  />
-                </label>
-                <input type="submit" className="button" value="login" />
+      <div className="page-wrapper">
+        <div className="primary-content marRight30">
+          <div className="mid-panel">
+            <div className="mid-panel-content">
+              <div className="title">
+                <h1>login</h1>
               </div>
-            </form>
-            <div className="clearing" />
-            <div className="address">
-              <div className="panel">
-                <div className="content">
-                  <span>If you don't have a profile click:</span> <Link to="/register">Here</Link>
+              <div className="border" />
+              <h2>Please, login</h2>
+              <form onSubmit={onSubmit}>
+                <div className="contact-form margin-top">
+                  <label>
+                    <span>Email</span>
+                    <input
+                      className="input_text"
+                      id="email"
+                      name={loginFormKeys.email}
+                      type="text"
+                      onChange={onChange}
+                      value={values[loginFormKeys.email]}
+                    />
+                  </label>
+                  <label>
+                    <span>Password</span>
+                    <input
+                      className="input_text"
+                      id="password"
+                      name={loginFormKeys.password}
+                      type="password"
+                      onChange={onChange}
+                      value={values[loginFormKeys.password]}
+                    />
+                  </label>
+                  <input type="submit" className="button" value="login" />
+                </div>
+              </form>
+              <div className="clearing" />
+              <div className="address">
+                <div className="panel">
+                  <div className="content">
+                    <span>If you don't have a profile click:</span> <Link to="/register">Here</Link>
+                  </div>
                 </div>
               </div>
             </div>
