c502ce5eb5281be9023c85f0c198b12bcdf269ab
AssiaIlieva <ac17@abv.bg>
2024-07-29 17:20:21 +0300

    Add Register functionality

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,5 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+import { useRegister } from '../../hooks/useAuth';
+import useForm from '../../hooks/useForm';
+
+const initialValues = { username: '', email: '', password: '', rePassword: '' };
 export default function Register() {
+  const [error, setError] = useState('');
+  const register = useRegister();
+  const navigate = useNavigate();
+
+  const registerHandler = async (values) => {
+    // if (values.password.length < 6) {
+    //   setError('Password must be at least 6 characters');
+    //   return;
+    // }
+    if (values.password !== values.rePassword) {
+      setError('Password Missmatch!');
+      return;
+    }
+    try {
+      await register(values.username, values.email, values.password);
+      navigate('/');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const { values, changeHandler, submitHandler } = useForm(initialValues, registerHandler);
+
   return (
     <div className="top-content">
       <div className="page-wrapper">
@@ -11,24 +40,45 @@ export default function Register() {
               </div>
               <div className="border" />
               <h2>Please, fill in all the fields</h2>
-              <form>
+              <form onSubmit={submitHandler}>
                 <div className="contact-form margin-top">
                   <label>
                     <span>Full name</span>
-                    <input className="input_text" id="name" name="name" type="text" />
+                    <input className="input_text" id="username" name="username" value={values.username} onChange={changeHandler} type="text" />
                   </label>
                   <label>
                     <span>Email</span>
-                    <input className="input_text" id="email" name="email" type="text" />
+                    <input className="input_text" id="email" name="email" value={values.email} onChange={changeHandler} type="text" />
                   </label>
                   <label>
                     <span>Password</span>
-                    <input className="input_text" id="password" name="password" type="password" placeholder="At least 6 characters" />
+                    <input
+                      className="input_text"
+                      id="password"
+                      name="password"
+                      value={values.password}
+                      onChange={changeHandler}
+                      type="password"
+                      placeholder="At least 6 characters"
+                    />
                   </label>
                   <label>
                     <span>Repeat Password</span>
-                    <input className="input_text" id="rePassword" name="rePassword" type="password" />
+                    <input
+                      className="input_text"
+                      id="rePassword"
+                      name="rePassword"
+                      value={values.rePassword}
+                      onChange={changeHandler}
+                      type="password"
+                    />
                   </label>
+                  {error && (
+                    <label>
+                      <span>Error Message</span>
+                      <p style={{ fontSize: '16px', color: 'red' }}>{error}</p>
+                    </label>
+                  )}
                   <input type="submit" className="button" value="register" />
                 </div>
               </form>
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -9,6 +9,17 @@ export const useLogin = () => {
   const loginHandler = async (email, password) => {
     const result = await login(email, password);
     changeAuthState(result);
+    return result;
   };
   return loginHandler;
+};
+
+export const useRegister = () => {
+  const { changeAuthState } = useContext(AuthContext);
+  const registerHandler = async (username, email, password) => {
+    const result = await register(username, email, password);
+    changeAuthState(result);
+    return result;
+  };
+  return registerHandler;
 };
