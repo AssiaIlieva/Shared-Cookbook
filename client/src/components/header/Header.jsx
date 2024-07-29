130750ce97ddb0db631581476d1d39be3400b1a4
AssiaIlieva <ac17@abv.bg>
2024-07-29 18:11:32 +0300

    Add dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../contexts/AuthContext';
+
 export default function Header() {
+  const { username, isAuthenticated } = useContext(AuthContext);
   return (
     <div className="header">
       <div className="header-left-panel">
@@ -12,13 +16,7 @@ export default function Header() {
         </div>
       </div>
       <div className="header-right-panel">
-        <div className="header-right-panel-top">
-          <p>Call Us : 000 888 8888</p>
-          <p>
-            {' '}
-            <Link to="#">Mail Us : info@websitename.com</Link>
-          </p>
-        </div>
+        <div className="header-right-panel-top">{isAuthenticated ? <p>Hello, {username}!</p> : <p>Hello, Guest!</p>}</div>
         <div className="menu">
           <ul>
             <li className="marRight20">
@@ -32,36 +30,43 @@ export default function Header() {
               </NavLink>
             </li>
             <li className="marRight20">
-              <NavLink to="/recipes/create" activeclassname="active">
-                create recipe
-              </NavLink>
-            </li>
-            <li className="marRight20">
               <NavLink to="/tips" activeclassname="active">
                 tips
               </NavLink>
             </li>
-            <li className="marRight20">
-              <NavLink to="/tips/create" activeclassname="active">
-                create tip
-              </NavLink>
-            </li>
-            <li className="marRight20">
-              <NavLink to="/login" activeclassname="active">
-                login
-              </NavLink>
-            </li>
-            <li className="marRight20">
-              <NavLink to="/register" activeclassname="active">
-                register
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/logout" activeclassname="active">
-                logout
-              </NavLink>
-            </li>
           </ul>
+          {isAuthenticated ? (
+            <ul>
+              <li className="marRight20">
+                <NavLink to="/recipes/create" activeclassname="active">
+                  create recipe
+                </NavLink>
+              </li>
+              <li className="marRight20">
+                <NavLink to="/tips/create" activeclassname="active">
+                  create tip
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/logout" activeclassname="active">
+                  logout
+                </NavLink>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li className="marRight20">
+                <NavLink to="/login" activeclassname="active">
+                  login
+                </NavLink>
+              </li>
+              <li className="marRight20">
+                <NavLink to="/register" activeclassname="active">
+                  register
+                </NavLink>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </div>
