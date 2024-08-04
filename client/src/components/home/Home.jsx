60b30fbdb3e60a52792b9623d9c22c11b7c74741
AssiaIlieva <ac17@abv.bg>
2024-08-04 20:52:21 +0300

    Add a loading feature in the Home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -6,6 +6,7 @@ import RecipeCard from '../recipeCard/RecipeCard';
 
 export default function Home() {
   const [lastRecipes, setLastRecipes] = useState([]);
+  const [loading, setLoading] = useState(true); // Добавено състояние за зареждане
   const { openModal } = useModal();
 
   useEffect(() => {
@@ -15,14 +16,34 @@ export default function Home() {
         setLastRecipes(result);
       } catch (error) {
         openModal(<div>{error.message}</div>);
+      } finally {
+        setLoading(false); // Сменяме състоянието на зареждане, след като данните са извлечени
       }
     })();
   }, [openModal]);
+
+  if (loading) {
+    return (
+      <div className="panel-wrapper" style={{ marginTop: '100px' }}>
+        <h1
+          style={{
+            textAlign: 'center',
+            color: '#414141', // Цвят на текста
+            backgroundColor: '#cf9359', // Фон
+            padding: '20px', // Отстояние около текста
+            borderRadius: '5px', // Закръглени ъгли
+          }}>
+          Loading...
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="panel-wrap">
         <div className="marBottom30">
-          <h1 style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '40px' }}>welcome to the shared cookbook</h1>
+          <h1 style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '40px' }}>Welcome to the Shared Cookbook</h1>
         </div>
         {lastRecipes.length > 0 ? (
           <h1 style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '34px' }}>Our Latest Recipes</h1>
