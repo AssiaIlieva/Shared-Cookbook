7a6a61eaa3fa7f018c4c5cc329145a57b9e7abe3
AssiaIlieva <ac17@abv.bg>
2024-08-05 10:36:34 +0300

    Add useTips hook

---
+++ client/src/hooks/useTips.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import tipsAPI from '../api/recipes-api';
+
+export function useCreateTip() {
+  const tipCreateHandler = (tipData) => tipsAPI.create(tipData);
+
+  return tipCreateHandler;
+}
+
+export function useGetAllTips() {
+  const [tips, setTips] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTips = async () => {
+      try {
+        const result = await tipsAPI.getAll();
+        setTips(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTips();
+  }, []);
+
+  return { tips, error, loading };
+}
