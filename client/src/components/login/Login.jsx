642a86ce75d9188aec99efa7b2f346ba1ba1ebe3
AssiaIlieva <ac17@abv.bg>
2024-08-06 21:17:36 +0300

    Updat view on Login and Register page

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -28,34 +28,32 @@ export default function Login() {
 
   return (
     <div className="top-content">
-      <div className="page-wrapper">
-        <div className="primary-content marRight30">
-          <div className="mid-panel">
-            <div className="mid-panel-content">
-              <div className="title">
-                <h1>login</h1>
+      <div className="primary-content marRight30">
+        <div className="mid-panel">
+          <div className="mid-panel-content">
+            <div className="title">
+              <h1>login</h1>
+            </div>
+            <div className="border" />
+            <h2>Please, login</h2>
+            <form onSubmit={submitHandler}>
+              <div className="contact-form margin-top">
+                <label>
+                  <span>Email</span>
+                  <input className="input_text" id="email" name="email" value={values.email} onChange={changeHandler} type="text" />
+                </label>
+                <label>
+                  <span>Password</span>
+                  <input className="input_text" id="password" name="password" value={values.password} onChange={changeHandler} type="password" />
+                </label>
+                <input type="submit" className="button" value="login" />
               </div>
-              <div className="border" />
-              <h2>Please, login</h2>
-              <form onSubmit={submitHandler}>
-                <div className="contact-form margin-top">
-                  <label>
-                    <span>Email</span>
-                    <input className="input_text" id="email" name="email" value={values.email} onChange={changeHandler} type="text" />
-                  </label>
-                  <label>
-                    <span>Password</span>
-                    <input className="input_text" id="password" name="password" value={values.password} onChange={changeHandler} type="password" />
-                  </label>
-                  <input type="submit" className="button" value="login" />
-                </div>
-              </form>
-              <div className="clearing" />
-              <div className="address">
-                <div className="panel">
-                  <div className="content">
-                    <span>If you don't have a profile click:</span> <Link to="/register">Here</Link>
-                  </div>
+            </form>
+            <div className="clearing" />
+            <div className="address">
+              <div className="panel">
+                <div className="content">
+                  <span>If you don't have a profile click:</span> <Link to="/register">Here</Link>
                 </div>
               </div>
             </div>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -39,57 +39,55 @@ export default function Register() {
 
   return (
     <div className="top-content">
-      <div className="page-wrapper">
-        <div className="primary-content marRight30">
-          <div className="mid-panel">
-            <div className="mid-panel-content">
-              <div className="title">
-                <h1>register</h1>
+      <div className="primary-content marRight30">
+        <div className="mid-panel">
+          <div className="mid-panel-content">
+            <div className="title">
+              <h1>register</h1>
+            </div>
+            <div className="border" />
+            <h2>Please, fill in all the fields</h2>
+            <form onSubmit={submitHandler}>
+              <div className="contact-form margin-top">
+                <label>
+                  <span>Full name</span>
+                  <input className="input_text" id="username" name="username" value={values.username} onChange={changeHandler} type="text" />
+                </label>
+                <label>
+                  <span>Email</span>
+                  <input className="input_text" id="email" name="email" value={values.email} onChange={changeHandler} type="text" />
+                </label>
+                <label>
+                  <span>Password</span>
+                  <input
+                    className="input_text"
+                    id="password"
+                    name="password"
+                    value={values.password}
+                    onChange={changeHandler}
+                    type="password"
+                    placeholder="At least 6 characters"
+                  />
+                </label>
+                <label>
+                  <span>Repeat Password</span>
+                  <input
+                    className="input_text"
+                    id="rePassword"
+                    name="rePassword"
+                    value={values.rePassword}
+                    onChange={changeHandler}
+                    type="password"
+                  />
+                </label>
+                <input type="submit" className="button" value="register" />
               </div>
-              <div className="border" />
-              <h2>Please, fill in all the fields</h2>
-              <form onSubmit={submitHandler}>
-                <div className="contact-form margin-top">
-                  <label>
-                    <span>Full name</span>
-                    <input className="input_text" id="username" name="username" value={values.username} onChange={changeHandler} type="text" />
-                  </label>
-                  <label>
-                    <span>Email</span>
-                    <input className="input_text" id="email" name="email" value={values.email} onChange={changeHandler} type="text" />
-                  </label>
-                  <label>
-                    <span>Password</span>
-                    <input
-                      className="input_text"
-                      id="password"
-                      name="password"
-                      value={values.password}
-                      onChange={changeHandler}
-                      type="password"
-                      placeholder="At least 6 characters"
-                    />
-                  </label>
-                  <label>
-                    <span>Repeat Password</span>
-                    <input
-                      className="input_text"
-                      id="rePassword"
-                      name="rePassword"
-                      value={values.rePassword}
-                      onChange={changeHandler}
-                      type="password"
-                    />
-                  </label>
-                  <input type="submit" className="button" value="register" />
-                </div>
-              </form>
-              <div className="clearing" />
-              <div className="address">
-                <div className="panel">
-                  <div className="content">
-                    <span>If you already have a profile click:</span> <Link to="/login">Here</Link>
-                  </div>
+            </form>
+            <div className="clearing" />
+            <div className="address">
+              <div className="panel">
+                <div className="content">
+                  <span>If you already have a profile click:</span> <Link to="/login">Here</Link>
                 </div>
               </div>
             </div>
