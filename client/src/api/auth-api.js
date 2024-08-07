a7c1ec67111c93aae3c1a441b330a0317043b36b
AssiaIlieva <ac17@abv.bg>
2024-08-07 22:22:22 +0300

    Update the url paths

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -1,6 +1,6 @@
 import requester from './requester';
 
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/users`;
+const BASE_URL = 'http://localhost:3030/users';
 
 export const login = (email, password) => requester.post(`${BASE_URL}/login`, { email, password });
 
